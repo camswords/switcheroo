82a10b238c80b14da8d1be955aee2b652e9a7a4c
Cam Swords <cam.swords@gmail.com>
2013-01-15 22:19:06 +1100

    initial commit to add the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Switcheroo::Application.config.secret_token = '8847ed28dc313c674ced5b6a51be0c92a920a89dac07e2284342cffebf102633601f51a524fddde53685f708d1e67db7cfef0450b8e622918618070e4aacc43c'
