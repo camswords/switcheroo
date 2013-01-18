8a90cd0d707a4e2141b1ace7208b29d21c38d21f
Cam Swords <cam.swords@gmail.com>
2013-01-19 00:24:15 +1100

    stop the program before deletion.

--- app/controllers/available_controller.rb
+++ app/controllers/available_controller.rb
@@ -5,5 +5,7 @@ class AvailableController < ActionController::Base
   
   def show
     @all_programs = Puppet::Module::JavaProgram.all_programs
+    running_programs = Puppet::Module::Agent.running_programs
+    @stopped_programs = @all_programs - running_programs
   end
 end
--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -14,12 +14,17 @@ class ProgramsController < ActionController::Base
   end
   
   def new
-    Puppet::Module::JavaProgram.save(params[:name], params[:artifact].tempfile, params[:artifact].original_filename)
-    flash[:notice] = "#{params[:name]} has been uploaded."
-    redirect_to "/available"
-  end
+    if !params[:name] || params[:name].blank? || !params[:artifact] || File.extname(params[:artifact].original_filename) != ".jar"
+      flash[:error] = "Please provide a name and an artifact. The artifact has to be a jar file."
+    else
+      Puppet::Module::JavaProgram.save(params[:name], params[:artifact].tempfile, params[:artifact].original_filename)
+      flash[:notice] = "#{params[:name]} has been uploaded."
+    end    
+      redirect_to "/available"
+    end
   
   def delete
+    Puppet::Module::Agent.stop_program(params[:program_name])
     Puppet::Module::JavaProgram.delete(params[:program_name])
     flash[:notice] = "#{params[:program_name]} has been deleted."
     redirect_to "/available"
