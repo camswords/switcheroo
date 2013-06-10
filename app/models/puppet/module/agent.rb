04d497e4d6d185e7c3e81e9ed38f6624b6a826d7
Cam Swords <cam.swords@gmail.com>
2013-06-10 15:53:41 +1000

    the puppet agent cannot be stopped.

--- app/models/puppet/module/agent.rb
+++ app/models/puppet/module/agent.rb
@@ -3,21 +3,25 @@ module Puppet
   module Module
       
     class Agent
-  
-      def self.running_programs
+
+      def self.all_running_programs
         server_tags_file = File.join(Rails.configuration.puppet_module_path, "puppet_agent/files/etc/puppet/server_tags")
         return [] unless File.exists?(server_tags_file)
         
         server_tags = IO.read(server_tags_file)
         server_tags.gsub(/\n|,/,"").split("role:").select { |role| role && !role.empty? }
       end
+
+      def self.running_programs
+        all_running_programs.select { |program_name| program_name != 'puppet_agent' }
+      end
       
       def self.start_program(program_name)
-        update_server_tags_with_programs(running_programs << program_name)
+        update_server_tags_with_programs(all_running_programs << program_name)
       end
 
       def self.stop_program(program_name)
-        update_server_tags_with_programs(running_programs - [program_name])
+        update_server_tags_with_programs(all_running_programs - [program_name])
       end
       
       def self.update_server_tags_with_programs(programs)
